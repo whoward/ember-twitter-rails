c875e770e913aaed335096083df22e401f5d1690
William Howard <whoward.tke@gmail.com>
2016-02-20 03:51:45 +0000

    add filters for tweets controllerto filter by user and hashtag

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :require_authentication, only: %i(create)
   
   def index
-    render json: Tweet.all, include: %w(author)
+    render json: collection, include: %w(author)
   end
   
   def create
@@ -14,5 +14,25 @@ class TweetsController < ApplicationController
   
   def permitted_params
     params.require(:data).require(:attributes).permit(:text)
+  end
+  
+  def collection
+    scope = Tweet.unscoped
+    
+    scope = user_scope(scope, params[:user]) if params[:user].present?
+    
+    scope = hashtag_scope(scope, params[:hashtag]) if params[:hashtag].present?
+    
+    scope
+  end
+  
+  def user_scope(scope, handle)
+    user_id = User.where(handle: handle).pluck(:id).first
+    
+    scope.where(user_id: user_id)
+  end
+  
+  def hashtag_scope(scope, tag)
+    scope.where('text ilike ?', "%##{tag}%")
   end
 end
